70dbb1cc9014512abee964f33e86bc883f9deead
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2024-05-22 10:19:16 +1200

    Add files via upload

---
+++ Fence calculator.py
@@ -0,0 +1,48 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def num_check(question):
+    error = "Please enter a number that is more than zero/n"
+    while True:
+
+        try:
+                # ask the user for a number
+                response = float(input(question))
+
+                # check that the number is more than zero
+                if response > 0:
+                   return response
+                else:
+                   print(error)
+
+
+        except ValueError:
+             print(error)
+# Main Rountine starts here
+
+
+keep_going = ""
+while keep_going == "":
+
+    # Get width and height  
+    width = num_check("Width: ")
+    height = num_check("Height: ")
+
+    # enter the cost per meter
+    cost = int(input("Cost per meter: "))
+        
+    # Caculate  perimeter
+    perimeter = 2 * (width + height)
+
+    # Caculate cost
+    cost = (perimeter * cost)
+
+    # Display output
+    print()
+    print(f"Cost: ${cost} ")
+    print(f"Perimeter: {perimeter}m ")
+    
+    # Ask user if they want to keep going
+    keep_going = input("Press enter to keep going or any key to quit. ")
+    print()
+
+print("Thank you for using the area / perimeter calculator")
